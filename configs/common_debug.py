be7af9d22301896cf42af32b67a057298597f4e4
Pavlo Ryndin <meg4mag@gmail.com>
2021-05-22 17:42:29 +0300

    modified configs

--- configs/common_debug.py
+++ configs/common_debug.py
@@ -6,9 +6,9 @@ RUN_INSERT = False
 
 SECRET_KEY = secrets.token_hex(16)
 
-database_pass = "totalmag"
+database_pass = "qwerty"
 shema_link = "localhost/department_project"
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:{}@{}'.format(database_pass, shema_link)
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://db_user:{}@{}'.format(database_pass, shema_link)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
