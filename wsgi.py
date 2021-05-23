ea1ab45ac28cea1f15a690cf98570e51bc35a217
PLOheMfW <meg4mag@gmail.com>
2021-05-23 13:13:28 +0300

    coveralls fix

--- wsgi.py
+++ wsgi.py
@@ -2,14 +2,11 @@ from src import app
 
 # TODO
 
-# 5 a lot of theory
+# 3 a lot of theory
 
-# 4 отдельная документация с переделать concept.jpg и создать ER-диаграмму
+# 2 отдельная документация с переделать concept.jpg и создать ER-диаграмму
 
-# travis       fix later
-# coveralls.io
-
-# 2 gunikorn
+# 1 gunikorn
 
 #app starting here
 if __name__ == "__main__":
